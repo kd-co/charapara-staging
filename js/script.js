d89c5bfb7641adb47e66d51ce4b43ec76cec41bc
Anoop Sethumadhavan <anoop4sethu@gmail.com>
2018-05-23 20:56:52 +0530

    Added new PNGs

--- js/script.js
+++ js/script.js
@@ -116,7 +116,9 @@ function randomizeLogoBlocksVerticalNew(){
 }
 
 function randomizeLogoBlocksOnClick(){
-	$('.card').toggleClass('card-flip-vertical');
+
+	$('.card-stack').toggleClass('card-flip-vertical-1');
+
 
 	var panels = [1, 2, 3, 4];
 
@@ -131,17 +133,23 @@ function randomizeLogoBlocksOnClick(){
 
 	setTimeout(function(){
 		for(var i=1; i<=4; i++){
-			if($('#card-wrapper').data('flip')==='front'){
-				$('#card-wrapper').data('flip', 'back');
-			} else {
-				$('#card-wrapper').data('flip', 'front');
-			}
 			
 			temp_item = panels[Math.floor(Math.random()*panels.length)];
 
-			console.log(temp_item, imgs[temp_item-1]);
+			console.log(temp_item, imgs[temp_item-1], $('#card-wrapper').data('flip'));
 
-			$('#card-flip-' + i).find('.back-image').attr('src', imgs[temp_item-1].src);
+			if($('#card-wrapper').data('flip')==='front'){
+				$('#card-flip-' + i).find('.back-image').attr('src', imgs[temp_item-1].src);
+				setTimeout(function(){
+					$('#card-wrapper').data('flip', 'back');
+				});
+			} else {
+				$('#card-flip-' + i).find('.front-image').attr('src', imgs[temp_item-1].src);
+				setTimeout(function(){
+					$('#card-wrapper').data('flip', 'front');
+				});
+			}
+
 
 			panels = removeItem(panels, temp_item);
 			
@@ -151,6 +159,61 @@ function randomizeLogoBlocksOnClick(){
 		}
 	}, 100);	
 }
+
+
+function randomizeLogoBlocksOnClick2(){
+
+	$('.card-line').toggleClass('card-flip-vertical-1');
+
+
+	var panels = [1, 2, 3, 4];
+
+	var imgs = [
+		{ src: 'img/cha.png'},
+		{ src: 'img/ra.png'},
+		{ src: 'img/pa.png'},
+		{ src: 'img/ra.png'},
+	];
+
+	var img, temp_item;
+
+	setTimeout(function(){
+		for(var i=1; i<=4; i++){
+			if($('#card-wrapper-2').data('flip')==='front'){
+				$('#card-wrapper-2').data('flip', 'back');
+			} else {
+				$('#card-wrapper-2').data('flip', 'front');
+			}
+			
+			temp_item = panels[Math.floor(Math.random()*panels.length)];
+
+			console.log(temp_item, imgs[temp_item-1]);
+
+			if($('#card-wrapper-2').data('flip')==='front'){
+				$('#card-flip-line-' + i).find('.back-image').attr('src', imgs[temp_item-1].src);
+				setTimeout(function(){
+					$('#card-wrapper-2').data('flip', 'back');
+				});
+			} else {
+				$('#card-flip-line-' + i).find('.front-image').attr('src', imgs[temp_item-1].src);
+				setTimeout(function(){
+					$('#card-wrapper-2').data('flip', 'front');
+				});
+			}
+
+			
+
+			panels = removeItem(panels, temp_item);
+			
+
+			console.log("After deletion, ", panels);
+
+		}
+	}, 100);	
+}
+
+
+
 
 function logoDefaultState(){
 	$('.logo-block').removeClass("logo-block-pos-1 logo-block-pos-2 logo-block-pos-3 logo-block-pos-4");
